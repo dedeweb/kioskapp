a0f158667325968ce631e58fe9779304b70dc65d
MESSIE Denis <dmessie@intranet.citranet.fr>
2017-01-20 16:47:34 +0000

    adding CORS

--- platforms/android/assets/www/jxcore/app.js
+++ platforms/android/assets/www/jxcore/app.js
@@ -7,6 +7,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+	next();
+});
+
 app.get('/version', function (req, res) {
 	Mobile('getVersion').call(function (version){
 		res.status(200).send({ app: 'kioskApp', version: version});
