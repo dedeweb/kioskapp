b17dc37d6164bca398201ab75920a2d590485093
Denis <denis.messie@gmail.com>
2018-06-08 12:50:23 +0200

    cleanup + travis CI  fix

--- hooks/before_build/custom_gradle.sh
+++ hooks/before_build/custom_gradle.sh
@@ -2,10 +2,10 @@
 
 if [ -d "platforms/android" ]; then
 
-echo "copying extra gradle configuration to android directory"
-cp resources/build-extras.gradle platforms/android/build-extras.gradle
+  #echo "copying extra gradle configuration to android directory"
+  #cp resources/build-extras.gradle platforms/android/build-extras.gradle
 
-echo "OVERWRITING GENERATED build.gradle IN PROJECT, AS LONG AS WE DO    NOT FIND A BETTER WAY"
-cp resources/build.gradle platforms/android/build.gradle
+  echo "OVERWRITING GENERATED build.gradle IN PROJECT, AS LONG AS WE DO    NOT FIND A BETTER WAY"
+  cp resources/build.gradle platforms/android/build.gradle
 
 fi
