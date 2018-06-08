b3282ee97f97c5bf03382486b7c466e737826513
Denis <denis.messie@gmail.com>
2018-06-08 10:59:09 +0200

    try to commit apk generated from travis ci

---
+++ travis_upload_apk.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#https://gist.github.com/willprice/e07efd73fb7f13f917ea
+#https://medium.com/@daggerdwivedi/push-your-apk-to-your-github-repository-from-travis-11e397ec430d
+built_apk_dir = $TRAVIS_BUILD_DIR/platforms/android/build/outputs/apk
+
+copy_files() {
+  cp -R $built_apk_dir $TRAVIS_BUILD_DIR/apk/ 
+}
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_apk_files() {
+  git add apk
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER [skip ci]"
+}
+
+upload_files() {
+  git remote add origin https://${GITHUB_KEY}@github.com/dedeweb/kioskapp.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+if [ ! -f $built_apk_dir/android-armv7-release-unsigned.apk ] || [ ! -f $built_apk_dir/android-x86-release-unsigned.apk ]; then
+    echo -e "Error : build file not present :( \n in $built_apk_dir"
+    exit 1
+fi
+
+copy_files
+setup_git
+commit_apk_files
+upload_files
